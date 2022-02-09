8dbf335e9b9984778220034ef39b4ca3b784fd50
firasalbarazi <albaraziferas@gmail.com>
2022-02-09 21:14:55 +0100

    Initial commit

---
+++ uebung.js
@@ -0,0 +1,24 @@
+let beliebigerString = "Hallo"
+let eineZahl = 1 ;
+let nochEineZahl = 4 ;
+let ergebnis ;
+let eineZahlMitKomma = 5 ;
+
+
+
+     function rechenspiele () {
+             ergebnis = eineZahl * nochEineZahl
+             if (ergebnis > eineZahlMitKomma) {
+                     console.log(ergebnis - eineZahlMitKomma);
+             } else {
+                     console.log("Das Ergebnis ist zu klein")
+             }
+     }
+     
+     function ungerade() { for (int i = ergebnis; i<=10; i++){
+         if (i/2!=0)
+             console.log(i)
+     }
+             
+     }
+
